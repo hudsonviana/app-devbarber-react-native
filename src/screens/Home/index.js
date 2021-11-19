910aa4d5233d53c61028f33a1fb7420ef135fdf0
hudsonviana <hudson1206@gmail.com>
2021-11-19 01:08:18 -0300

    Tela Home - primeira parte
    Configurações iniciais da tela home.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { 
+    Container,
+    Scroller,
+    HeaderArea,
+    HeaderTitle,
+    SearchButton,
+    LocationArea,
+    LocationInput,
+    LocationFinder 
+} from "./styles";
+
+import SearchIcon from '../../assets/search.svg';
+import MylocationIcon from '../../assets/my_location.svg';
 
 export  default () => {
+
+    const navigation = useNavigation();
+    
     return (
         <Container>
-            <Text>Home</Text>
+            <Scroller>
+                <HeaderArea>
+                    <HeaderTitle numberOfLines={2}>Encontre o seu barbeiro favorito</HeaderTitle>
+                    <SearchButton onPress={()=>navigation.navigate('Search')}>
+                        <SearchIcon width="26" height="26" fill="#ffffff"></SearchIcon>
+                    </SearchButton>
+                </HeaderArea>
+
+                <LocationArea>
+                    <LocationInput />
+                    <LocationFinder>
+                        <MylocationIcon width="24" height="24" fill="#ffffff" />
+                    </LocationFinder>
+                </LocationArea>
+            </Scroller>
         </Container>
     );
 }
